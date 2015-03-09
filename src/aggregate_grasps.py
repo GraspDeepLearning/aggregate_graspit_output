1b0677ddf6df6d042d99c42d17dc25246b49fed6
jvarley <jakevarley@gmail.com>
2015-03-09 11:34:51 -0400

    setup test framework, added setup.py removed dead files

---
+++ src/aggregate_grasps.py
@@ -0,0 +1,57 @@
+
+import os
+import rospkg
+
+from grasp import get_model_grasps
+from grasp_dataset import GraspDataset
+
+from choose import choose_from
+from date_string import get_date_string
+from paths import RAW_GRASPIT_DIR, AGG_GRASPIT_DIR
+
+
+class GraspAggregator():
+
+    def __init__(self, graspit_grasps_dir, graspit_agg_dir, grasp_dataset, energy_threshold=-.25):
+        self.graspit_grasps_dir = graspit_grasps_dir
+        self.graspit_agg_dir = graspit_agg_dir
+        self.grasp_dataset = grasp_dataset
+        self.energy_threshold = energy_threshold
+
+    def run(self):
+        grasps = []
+        print "reading grasps from files"
+        for model_name in os.listdir(self.graspit_grasps_dir):
+            grasps = grasps + get_model_grasps(self.graspit_grasps_dir + model_name,
+                                               model_name,
+                                               graspClass=self.grasp_dataset.Grasp)
+
+        print "writing low energy grasps to h5"
+        num = 0
+        for grasp in grasps:
+            if num % 1000 is 0:
+                print "%s / %s" % (num, len(grasps))
+
+            num += 1
+
+            if grasp.energy < self.energy_threshold:
+                self.grasp_dataset.add_grasp(grasp)
+
+
+if __name__ == "__main__":
+
+    graspit_dir = choose_from(RAW_GRASPIT_DIR)
+    graspit_grasps_dir = RAW_GRASPIT_DIR + graspit_dir + '/'
+
+    rospack = rospkg.RosPack()
+    DATASET_TEMPLATE_PATH = rospack.get_path('grasp_dataset')
+
+    grasp_dataset = GraspDataset(AGG_GRASPIT_DIR + graspit_dir + "-" + get_date_string() + ".h5",
+                                 DATASET_TEMPLATE_PATH + "/dataset_configs/graspit_grasps_dataset.yaml")
+
+    grasp_aggregator = GraspAggregator(graspit_grasps_dir, AGG_GRASPIT_DIR, grasp_dataset)
+    grasp_aggregator.run()
+
+    import IPython
+    IPython.embed()
+    assert False
---
+++ test/test_aggregrate_grasps.py
@@ -0,0 +1,44 @@
+
+import unittest
+import rospkg
+import os
+
+from grasp_dataset import GraspDataset
+from paths import TEST_RAW_GRASPIT_DIR, TEST_AGG_GRASPIT_DIR
+
+from aggregate_grasps import GraspAggregator
+
+
+class TestAggregateGrasps(unittest.TestCase):
+
+    def setUp(self):
+
+        graspit_dir = "contact_and_potential_grasps"
+        graspit_grasps_dir = TEST_RAW_GRASPIT_DIR + graspit_dir + '/'
+
+        rospack = rospkg.RosPack()
+        DATASET_TEMPLATE_PATH = rospack.get_path('grasp_dataset')
+
+        self.output_file = TEST_AGG_GRASPIT_DIR + graspit_dir + ".h5"
+
+        if os.path.exists(self.output_file):
+            os.remove(self.output_file)
+
+        grasp_dataset = GraspDataset(self.output_file ,
+                                     DATASET_TEMPLATE_PATH + "/dataset_configs/graspit_grasps_dataset.yaml")
+
+        energy_threshold = -0.25
+        self.grasp_aggregator = GraspAggregator(graspit_grasps_dir, TEST_AGG_GRASPIT_DIR, grasp_dataset, energy_threshold)
+
+    def test_output_dim(self):
+        self.grasp_aggregator.run()
+        self.assertEqual(self.grasp_aggregator.grasp_dataset.get_current_index(), 3629)
+
+    def tearDown(self):
+        if os.path.exists(self.output_file):
+            os.remove(self.output_file)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
